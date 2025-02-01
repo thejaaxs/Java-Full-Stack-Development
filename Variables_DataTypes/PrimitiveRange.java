28eda5c6e195ab5a257d4c14b609c2c8dda65b5f
Thejas <thejasganiga150503@gmail.com>
2025-02-01 18:28:49 +0530

    Primitive DataTypes & Ranges

--- Variables_DataTypes/PrimitiveRange.java
+++ Variables_DataTypes/PrimitiveRange.java
@@ -1,8 +1,11 @@
+package Variables_DataTypes ;
+
 // The List of all Data Types and their maximum and the minimum Ranges ! 
 // The Data Types are : Byte , Integer , Character , Double , Float , Short , Long etc ! 
 
 public class PrimitiveRange {
     public static void main(String[] args) {
         System.out.println("The Byte Range : "+"Maximum : "+Byte.MAX_VALUE+" & Minimum : "+Byte.MIN_VALUE);
+        System.out.println("The Integer Range : "+"Maximum : "+Integer.MAX_VALUE+" & Minimum : "+Integer.MIN_VALUE);
     }
 }
