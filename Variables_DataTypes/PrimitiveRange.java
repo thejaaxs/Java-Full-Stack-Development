b29f37615d68acab82594f08d01c56bedf50dfb3
Thejas <thejasganiga150503@gmail.com>
2025-02-01 18:32:22 +0530

    Data Types & Ranges

--- Variables_DataTypes/PrimitiveRange.java
+++ Variables_DataTypes/PrimitiveRange.java
@@ -7,5 +7,6 @@ public class PrimitiveRange {
     public static void main(String[] args) {
         System.out.println("The Byte Range : "+"Maximum : "+Byte.MAX_VALUE+" & Minimum : "+Byte.MIN_VALUE);
         System.out.println("The Integer Range : "+"Maximum : "+Integer.MAX_VALUE+" & Minimum : "+Integer.MIN_VALUE);
+        System.out.println("This Float Range : "+"Maximum : "+Float.MAX_VALUE+" & Minimum : "+Float.MIN_VALUE);
     }
 }
