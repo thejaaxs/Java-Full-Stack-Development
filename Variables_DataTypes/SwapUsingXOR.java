d76c4479085127921b3b6345b540175a71fa3eab
Thejas <thejasganiga150503@gmail.com>
2025-02-01 22:45:41 +0530

    Swapping of 2 using the XOR

---
+++ Variables_DataTypes/SwapUsingXOR.java
@@ -0,0 +1,23 @@
+// Swappping of two numbers using the XOR !
+
+package Variables_DataTypes;
+
+import java.util.Scanner ;
+public class SwapUsingXOR {
+    static void swap(int a , int b){
+        a = a ^ b ;
+        b = a ^ b ;
+        a = a ^ b ;
+        System.out.println("After Swapping : a : "+a+" & b : "+b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second Number : ");
+        int b = sc.nextInt();
+        System.out.println("Before Swapping : a : "+a+" & b : "+b);
+        swap(a,b);
+        sc.close();
+    }
+}
