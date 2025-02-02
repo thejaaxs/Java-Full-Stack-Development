fda55790b689f66b3fd7ef1f9739425823905b02
Thejas <thejasganiga150503@gmail.com>
2025-02-03 01:09:37 +0530

    Triangle Pattern

---
+++ Loops_Conditionals/TrianglePattern.java
@@ -0,0 +1,20 @@
+package Loops_Conditionals;
+
+import java.util.Scanner ;
+public class TrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<(n-i);j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=(2*i-1);j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
