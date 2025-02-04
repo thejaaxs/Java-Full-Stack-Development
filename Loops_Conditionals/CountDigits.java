4ab99c60b799bb370f86ac21e04bcb9e182ac4d2
Thejas <thejasganiga150503@gmail.com>
2025-02-04 11:29:30 +0530

    Count the digits in a Number

---
+++ Loops_Conditionals/CountDigits.java
@@ -0,0 +1,20 @@
+package Loops_Conditionals ;
+
+import java.util.Scanner ;
+public class CountDigits {
+    public static int countDig(int num){
+        int count = 0 ;
+        while(num!=0){
+            num /= 10 ;
+            count++ ;
+        }
+        return count ;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int num = sc.nextInt();
+        System.out.println("The Total number of Digits : "+countDig(num));
+        sc.close();
+    }
+}
