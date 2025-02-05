c99e34b2f296a1d113c2bb08966ac1c66731f9ab
Thejas <thejasganiga150503@gmail.com>
2025-02-05 18:45:21 +0530

    Sum of Even / Odd Digits in a Number

---
+++ Loops_Conditionals/SumEvenOddDigits.java
@@ -0,0 +1,31 @@
+package Loops_Conditionals;
+
+import java.util.Scanner ;
+public class SumEvenOddDigits {
+    static int oddSum(int num){
+        int oddS = 0 ;
+        while(num!=0){
+            int lastDig = num%10;
+            if(lastDig%2!=0) oddS+=lastDig ;
+            num /= 10 ;
+        }
+        return oddS ;
+    }
+    static int evenSum(int num){
+        int evenS = 0 ;
+        while(num!=0){
+            int lastDig = num%10;
+            if(lastDig%2==0) evenS+=lastDig ;
+            num/=10;
+        }
+        return evenS ;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int num = sc.nextInt();
+        System.out.println("The Even Sum is : "+evenSum(num));
+        System.out.println("The Odd Sum is : "+oddSum(num));
+        sc.close();
+    }
+}
