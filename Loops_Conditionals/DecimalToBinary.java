bfc927e56f150a098d331d82f37b4dcbed0d5ef7
Thejas <thejasganiga150503@gmail.com>
2025-02-05 20:23:53 +0530

    Decimal to Binary Conversion

---
+++ Loops_Conditionals/DecimalToBinary.java
@@ -0,0 +1,22 @@
+//  To convert the Decimal to Binary 
+
+package Loops_Conditionals ;
+
+import java.util.Scanner ;
+public class DecimalToBinary {
+    static String decimalToBinary(int num){
+        StringBuffer binary = new StringBuffer();
+        while(num!=0){
+            binary.append(num%2) ;
+            num/=2 ;
+        }
+        return binary.reverse().toString() ;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int n = sc.nextInt();
+        System.out.println("The Binary : "+decimalToBinary(n));
+        sc.close();
+    }
+}
