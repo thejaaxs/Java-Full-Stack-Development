8a8addcce7a309b1e9270839a2cbf70074940523
Thejas <thejasganiga150503@gmail.com>
2025-02-05 18:35:21 +0530

    Printing the Pascal Triangle

---
+++ Loops_Conditionals/PascalTriangle.java
@@ -0,0 +1,27 @@
+package Loops_Conditionals;
+
+import java.util.Scanner ;
+public class PascalTriangle {
+    static int fact(int n){
+        if(n==0) return 1 ;
+        return n * fact(n-1);
+    }
+    static int combination(int n , int r){
+        return fact(n) / (fact(r) * fact(n-r));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");  
+        int n =sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print(combination(i,j)+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
