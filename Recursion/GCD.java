84fe1bc98726c603844cd6b813898678e55abbec
Thejas <thejasganiga150503@gmail.com>
2025-02-07 23:59:41 +0530

    GCD Using Recursion

--- Recursion/GCD.java
+++ Recursion/GCD.java
@@ -4,8 +4,16 @@ package Recursion;
 
 import java.util.Scanner ;
 public class GCD {
+    static int gcd(int a , int b){
+        if(b==0) return a ;
+        return gcd(b,a%b);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 Numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("The GCD of 2 Numbers is : "+gcd(a,b));
         sc.close();
     }
 }
