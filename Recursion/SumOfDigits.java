74ce1c00cd0ea6af71eb8b813555362dcad6e71f
Thejas <thejasganiga150503@gmail.com>
2025-02-08 23:40:11 +0530

    Sum Of the Digits in a Number Using Recursion

--- Recursion/SumOfDigits.java
+++ Recursion/SumOfDigits.java
@@ -4,15 +4,15 @@ package Recursion;
 
 import java.util.Scanner ;
 public class SumOfDigits {
-    static int sumOfDig(int num,int sum){
-        if(num==0) return sum ;
-        return sumOfDig(num/10,sum+num%10);
+    static int sumOfDig(int num){
+        if(num==0) return 0 ;
+        return num%10 + sumOfDig(num/10);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number : ");
         int num = sc.nextInt();
-        System.out.println("The Sum of the Digits is : "+sumOfDig(num,0));
+        System.out.println("The Sum of the Digits is : "+sumOfDig(num));
         sc.close();
     }
 }
