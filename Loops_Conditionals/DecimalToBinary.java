52ff0aaaeb107a202e046b4e45fcf32fd89028f8
Thejas <thejasganiga150503@gmail.com>
2025-02-13 00:40:14 +0530

    Decimal to Binary

--- Loops_Conditionals/DecimalToBinary.java
+++ Loops_Conditionals/DecimalToBinary.java
@@ -7,8 +7,8 @@ public class DecimalToBinary {
     static String decimalToBinary(int num){
         StringBuffer binary = new StringBuffer();
         while(num!=0){
-            binary.append(num%2) ;
-            num/=2 ;
+                binary.append(num%2) ;
+                num/=2 ;
         }
         return binary.reverse().toString() ;
     }
