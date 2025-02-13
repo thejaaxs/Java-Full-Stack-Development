6bd1e82cab51534fa04e90a7761e745a99134c32
Thejas <thejasganiga150503@gmail.com>
2025-02-14 00:12:48 +0530

    Single Inheritance Using OOPS

--- OOPS_2/SingleInheritance.java
+++ OOPS_2/SingleInheritance.java
@@ -5,6 +5,11 @@ package OOPS_2;
 class Vehicle{
     int speed ; 
     int fuelCapacity ;
+
+    public Vehicle(int speed , int fuelCapacity){
+        this.speed = speed ;
+        this.fuelCapacity = fuelCapacity ;
+    }
 }
 public class SingleInheritance {
     public static void main(String[] args) {
