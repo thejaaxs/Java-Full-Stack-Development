e9e2df1a7bacffdcdb384b9f7627626f58952224
Thejas <thejasganiga150503@gmail.com>
2025-02-24 13:16:36 +0530

    Single Inheritance

--- OOPS_2/SingleInheritance.java
+++ OOPS_2/SingleInheritance.java
@@ -1,18 +1,23 @@
-// Program to Demonstrate the Single Inheritance
-
 package OOPS_2;
+// Parent Class
+class Animal {
+    String name ;
 
-class Vehicle{
-    int speed ; 
-    int fuelCapacity ;
-
-    public Vehicle(int speed , int fuelCapacity){
-        this.speed = speed ;
-        this.fuelCapacity = fuelCapacity ;
+    void eat(){
+        System.out.println(name+" is eating !");
+    }
+}
+// Child Class
+class Dog extends Animal {
+    void bark(){
+        System.out.println(name +" is barking !");
     }
 }
 public class SingleInheritance {
     public static void main(String[] args) {
-        
+        Dog d = new Dog();
+        d.name = "Jimmy";
+        d.eat();
+        d.bark();
     }
 }
