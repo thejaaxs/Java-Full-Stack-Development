fdcdd247ea0b9e1ed325feeb562941afda3e0319
Thejas <thejasganiga150503@gmail.com>
2025-03-01 01:02:58 +0530

    Vehicle Classification Management Using OOPS

--- OOPS_2/VehicleExample.java
+++ OOPS_2/VehicleExample.java
@@ -2,24 +2,24 @@
 
 package OOPS_2;
 
-class Vehicle{
+class Vehicle3{
     void start(){
         System.out.println("Vehicle is Starting !");
     }
 }
-class Car extends Vehicle{
+class Car3 extends Vehicle3{
     void drive(){
         System.out.println("Car is Driving !");
     }
 }
-class ElectricCar extends Car{
+class ElectricCar3 extends Car3{
     void chargeBattery(){
         System.out.println("Battery is Charging !");
     }
 }
 public class VehicleExample {
     public static void main(String[] args) {
-        ElectricCar tesla = new ElectricCar();
+        ElectricCar3 tesla = new ElectricCar3();
         tesla.start();
         tesla.drive();
         tesla.chargeBattery();
