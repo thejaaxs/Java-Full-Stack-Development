5acc35067cbed51c01f22cedcde6c85553946080
Thejas <thejasganiga150503@gmail.com>
2025-02-28 23:10:39 +0530

    Inheritance Example Using OOPS

---
+++ OOPS/InheritanceExample.java
@@ -0,0 +1,38 @@
+// Program of Inheritance Using OOPS 
+
+package OOPS;
+
+class Vehicle {
+    String type;
+    int speed;
+
+    public Vehicle(String type, int speed) {
+        this.type = type;
+        this.speed = speed;
+    }
+
+    public void show() {
+        System.out.println("Type: " + type + "\nSpeed: " + speed);
+    }
+}
+
+class Car extends Vehicle {
+    String brand;
+
+    public Car(String type, int speed, String brand) {
+        super(type, speed);
+        this.brand = brand;
+    }
+
+    public void carShow() {
+        show();
+        System.out.println("Brand: " + brand);
+    }
+}
+
+public class InheritanceExample {
+    public static void main(String[] args) {
+        Car c1 = new Car("Four Wheeler", 300, "BMW");
+        c1.carShow();
+    }
+}
