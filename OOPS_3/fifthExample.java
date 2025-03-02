dae223df4dd9b30a128d634615d70485769767e3
Thejas <thejasganiga150503@gmail.com>
2025-03-02 23:48:58 +0530

    Payment System Example Using Polymorphism

--- OOPS_3/fifthExample.java
+++ OOPS_3/fifthExample.java
@@ -3,6 +3,11 @@
 interface Payment{
     void pay(int amount);
 }
+class CreditCardPayments implements Payment{
+    public void pay(int amount){
+        System.out.println("Paid "+amount+" using Credit Card !");
+    }
+}
 public class fifthExample {
     public static void main(String[] args) {
         
