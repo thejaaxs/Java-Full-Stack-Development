8de24d5954c40483e8eb4c32fbdf44773c7923d5
Thejas <thejasganiga150503@gmail.com>
2025-03-02 23:58:00 +0530

    Payment System Example Using Polymorphism

--- OOPS_3/fifthExample.java
+++ OOPS_3/fifthExample.java
@@ -1,15 +1,23 @@
 // Payment Class Using OOPS 
 
-interface Payment{
+interface Payments{
     void pay(int amount);
 }
-class CreditCardPayments implements Payment{
+class CreditCardPayments implements Payments{
     public void pay(int amount){
         System.out.println("Paid "+amount+" using Credit Card !");
     }
 }
+class UPIPayment implements Payments{
+    public void pay(int amount){
+        System.out.println("Paid "+amount+" using UPI Payment !");
+    }
+}
 public class fifthExample {
     public static void main(String[] args) {
-        
+        Payments payment = new CreditCardPayments();
+        payment.pay(100);
+        payment = new UPIPayment();
+        payment.pay(500);
     }
 }
