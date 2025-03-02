cf821689bb4478c2525d4fa7693f36ca63e33001
Thejas <thejasganiga150503@gmail.com>
2025-03-02 23:17:17 +0530

    Shape Example Using Polymorphism

--- OOPS_3/thirdExample.java
+++ OOPS_3/thirdExample.java
@@ -1,3 +1,25 @@
+// Shape Example Using OOPS
+
+abstract class Shapes{
+   abstract void draw();
+}
+class Circle extends Shapes{
+    @Override
+    void draw(){
+        System.out.println("Circle is being Drawn !");
+    }
+}
+class Rectangle extends Shapes{
+    @Override
+    void draw(){
+        System.out.println("Rectangle is being Drawn !");
+    }
+}
 public class thirdExample {
-    
+    public static void main(String[] args) {
+        Shapes shape[] = {new Circle(),new Rectangle()};
+        for(Shapes s : shape){
+            s.draw();
+        }
+    }
 }
