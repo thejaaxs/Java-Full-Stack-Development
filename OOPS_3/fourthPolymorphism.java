11bc224b42cd6adbb15dde4f7dbdc54127e433f5
Thejas <thejasganiga150503@gmail.com>
2025-03-02 23:30:41 +0530

    Bank Account Example Using Polymorphism

---
+++ OOPS_3/fourthPolymorphism.java
@@ -0,0 +1,12 @@
+// Bank Account Example Using OOPS
+
+class BankAccount{
+    void calculateInterest(){
+        System.out.println("Interest");
+    }
+}
+public class fourthPolymorphism {
+    public static void main(String[] args) {
+        
+    }
+}
