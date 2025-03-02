bc0260012987dcf18580a4379a513f7dbe097e76
Thejas <thejasganiga150503@gmail.com>
2025-03-02 23:43:46 +0530

    Bank Account Example Using Polymorphism

--- OOPS_3/fourthPolymorphism.java
+++ OOPS_3/fourthPolymorphism.java
@@ -1,12 +1,25 @@
 // Bank Account Example Using OOPS
 
 class BankAccount{
-    void calculateInterest(){
-        System.out.println("Interest");
+    public void calculateInterest(){
+        System.out.println("Interest for the General Account !");
+    }
+}
+class SavingsAccount extends BankAccount{
+    public void calculateInterest(){
+        System.out.println("Calculate Interest for the Savings Account !");
+    }
+}
+class CurrentAccount extends BankAccount{
+    public void calculateInterest(){
+        System.out.println("Current Account has no Interest !");
     }
 }
 public class fourthPolymorphism {
     public static void main(String[] args) {
-        
+        BankAccount acc[] = {new SavingsAccount() , new CurrentAccount()};
+        for(BankAccount ac : acc){
+            ac.calculateInterest();
+        }
     }
 }
