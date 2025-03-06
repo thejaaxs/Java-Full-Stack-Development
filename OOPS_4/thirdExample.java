b52dfe2d3d8d47a0e3305708e8ffd9feefdb615a
Thejas <thejasganiga@gmail.com>
2025-03-07 00:48:54 +0530

    Rectangle Example using Encapsulation

---
+++ OOPS_4/thirdExample.java
@@ -0,0 +1,43 @@
+// Rectangle Example Using OOPS
+
+package OOPS_4;
+
+class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void calculateArea() {
+        System.out.println("Area : " + length * width);
+    }
+}
+
+public class thirdExample {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(10, 20);
+        rect.calculateArea();
+        rect.setLength(5);
+        rect.setWidth(10);
+        rect.calculateArea();
+    }
+}
