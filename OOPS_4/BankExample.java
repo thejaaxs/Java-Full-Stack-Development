5f22d116bce74ada0e35a6f537e32da857be9659
Thejas <thejasganiga@gmail.com>
2025-03-06 18:37:53 +0530

    Bank Account System Using Encapsulation

---
+++ OOPS_4/BankExample.java
@@ -0,0 +1,48 @@
+// Bank Example using OOPS
+
+package OOPS_4;
+
+class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+    public String getAccountHolder(){
+        return accountHolder;
+    }
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 00) {
+            balance += amount;
+            System.out.println("Deposited Amount is : " + amount);
+        } else
+            System.out.println("Amount must be in positive !");
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance && amount>0) {
+            balance -= amount;
+            System.out.println("Withdrawn Amount is : " + amount);
+        } else
+            System.out.println("Not Sufficient Fund !");
+    }
+}
+
+public class BankExample {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("Raghavendra",10000);
+
+        System.out.println("Holder Name : "+account.getAccountHolder());
+        System.out.println("The Current Balance is : "+account.getBalance());
+        account.deposit(10000);
+        System.out.println("Updated Balance is : "+account.getBalance());
+        account.withdraw(10000);
+        System.out.println("The Final Balance is : "+account.getBalance());
+    }
+}
