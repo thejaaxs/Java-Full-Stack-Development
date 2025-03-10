5811d3948855835cfded5c49edba8dd4dbdc9cf4
Thejas <thejasganiga150503@gmail.com>
2025-03-10 22:12:53 +0530

    Car Example using Encapsulation

--- OOPS_4/fourthExamples.java
+++ OOPS_4/fourthExamples.java
@@ -1,5 +1,11 @@
 // Car Example Using OOPS
 
+class Cars2{
+    private String brand;
+    private int speed;
+    private int fuelLevel;
+
+}
 public class fourthExamples{
     public static void main(String[] args){
         
